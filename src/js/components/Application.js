1cdf1d512b5226b6683d8d821c972d6a30a94891
Frank Douwes <info@frankdouwes.com>
2014-09-24 12:51:04 +0200

    initial commit

---
+++ src/js/components/Application.js
@@ -0,0 +1,10 @@
+/** @jsx React.DOM */
+var React = require('react');
+
+var Application =
+  React.createClass({
+    render:function(){
+      return <h1>my app</h1>
+    }
+  });
+module.exports = Application;
---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+/** @jsx React.DOM */
+var Application = require('./components/Application');
+var React = require('react');
+
+React.renderComponent(<Application />, document.getElementById('application'));
